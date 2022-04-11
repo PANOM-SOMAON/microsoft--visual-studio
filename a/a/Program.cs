7f74722b873a90d897888e64199aeedc367a17f5
PANOM-SOMAON <103121410+PANOM-SOMAON@users.noreply.github.com>
2022-04-11 19:42:15 +0700

    Add files via upload

---
+++ a/a/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace data
+{
+    class Program
+    {
+        static void Main(string[] args)
+       
+        {
+            int[] data =  int[] {1,2,3,4,5,6 };
+            foreach (int i in data)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
