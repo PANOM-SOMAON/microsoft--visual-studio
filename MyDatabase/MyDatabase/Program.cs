1c355bc6e42f881e9942f2cebdaaab02e1b80341
PANOM-SOMAON <103121410+PANOM-SOMAON@users.noreply.github.com>
2022-04-11 21:00:52 +0700

    Add files via upload

---
+++ MyDatabase/MyDatabase/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace MyDatabase
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            MyDB.showCustomer();
+           // MyDB.changeCustomerName();
+            //MyDB.changeCustomerID();
+           // MyDB.showCustomer();
+        }
+    }
+    class MyDB
+    {
+        static int customerID = 100;
+        static string strName = "Mr. Panom";
+        static string address = "999, 543Rd.";
+
+        public static void showCustomer()
+        {
+            Console.WriteLine("Customer ID is {0}", customerID);
+            Console.WriteLine("Customer name is {0}", strName);
+            Console.WriteLine("Customer address is {0}", address);
+
+        }
+       // public static void changeCustomerName()
+       // {
+           // Console.Write("Please Enter new Customer Name:");
+           // strName = Console.ReadLine();
+       // }
+
+        public static void changeCustomerID()
+        {
+            Console.Write("Please Enter new Customer ID:");
+            customerID = int.Parse(Console.ReadLine());
+        }
+        
+
+    }
+}
