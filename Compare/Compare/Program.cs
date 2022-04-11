b61fa2e2c5a5cc91948034eff7f139cf40a3c839
PANOM-SOMAON <103121410+PANOM-SOMAON@users.noreply.github.com>
2022-04-11 20:01:29 +0700

    Add files via upload

---
+++ Compare/Compare/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Number1 : ");
+            int A =int.Parse(Console.ReadLine());
+            Console.WriteLine("Number1 = {0}:", A);
+            Console.WriteLine("Enter Number2 : ");
+            int B = int.Parse(Console.ReadLine());
+            Console.WriteLine("Number2 = {0}:", B);
+            if (A < B)
+            {
+                Console.WriteLine("Number1 is more than number2");
+            }
+            else
+            {
+                Console.WriteLine("Number1 is less than number2");
+            }
+
+
+        }
+    }
+}
