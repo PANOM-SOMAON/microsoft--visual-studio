4a769d4a43df1e7e61baca79216fc0d3a1faf7aa
PANOM-SOMAON <103121410+PANOM-SOMAON@users.noreply.github.com>
2022-04-11 20:57:46 +0700

    Add files via upload

---
+++ LabH4.3/LabH4.3/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace LabH4._3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(40, 137);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ข้อมูลเมตริกช์ที่่ 1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(240, 137);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(130, 29);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "ข้อมูลเมตริกช์ที่ 2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(429, 137);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(138, 29);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "บวกเมตริกช์";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(614, 137);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(152, 29);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "ลบเมตริกช์";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(40, 216);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(139, 104);
+            this.listBox1.TabIndex = 4;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 20;
+            this.listBox2.Location = new System.Drawing.Point(240, 216);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(130, 104);
+            this.listBox2.TabIndex = 5;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 20;
+            this.listBox3.Location = new System.Drawing.Point(433, 216);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(134, 104);
+            this.listBox3.TabIndex = 6;
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.ItemHeight = 20;
+            this.listBox4.Location = new System.Drawing.Point(614, 216);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(152, 104);
+            this.listBox4.TabIndex = 7;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(307, 384);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(176, 39);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "เคลียร์ค่าทั้งหมด";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(40, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 28);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "ขนาดของแถว";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(209, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 27);
+            this.textBox1.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(445, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 28);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "ขนาดของหลัก";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(600, 50);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(125, 27);
+            this.textBox2.TabIndex = 12;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(209, 256);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 20);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "+";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(396, 256);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(19, 20);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "=";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private ListBox listBox1;
+        private ListBox listBox2;
+        private ListBox listBox3;
+        private ListBox listBox4;
+        private Button button5;
+        private Label label1;
+        private TextBox textBox1;
+        private Label label2;
+        private TextBox textBox2;
+        private Label label3;
+        private Label label4;
+    }
+}
---
+++ LabH4.3/LabH4.3/Form1.cs
@@ -0,0 +1,98 @@
+﻿namespace LabH4._3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int[,] Matrix1, Matrix2, AddMatrix;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int RowSize = int.Parse(textBox1.Text);
+            int ColSize = int.Parse(textBox2.Text);
+
+            Matrix1 = new int[RowSize, ColSize];
+            for (int i = 0; i < RowSize; ++i)
+            {
+                string rowTxt = "";
+                for (int j = 0; j < ColSize; ++j)
+                {
+                    string data = Microsoft.VisualBasic.Interaction.InputBox("แถวที่" + i + " หลักที่ " + j, "Matrix", "0", 0, 0);
+                    Matrix1[i, j] = int.Parse(data);
+                    rowTxt += data + "";
+
+                }
+                listBox1.Items.Add(rowTxt);
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int RowSize = int.Parse(textBox1.Text);
+            int ColSize = int.Parse(textBox2.Text);
+
+            Matrix2 = new int[RowSize, ColSize];
+            for (int i = 0; i < RowSize; ++i)
+            {
+                string rowTxt = "";
+                for (int j = 0; j < ColSize; ++j)
+                {
+                    string data = Microsoft.VisualBasic.Interaction.InputBox("แถวที่ " + i + " หลักที่ " + j, "Matrix2", "0", 0, 0);
+                    Matrix2[i, j] = int.Parse(data);
+                    rowTxt += data + "";
+                }
+                listBox2.Items.Add(rowTxt);
+
+            }
+        }
+
+            private void button3_Click(object sender, EventArgs e)
+        {
+            int RowSize = int.Parse(textBox1.Text);
+            int ColSize = int.Parse(textBox2.Text);
+
+            AddMatrix = new int[RowSize, ColSize];
+            for (int i = 0; i < RowSize; ++i)
+            {
+                string rowTxt = "";
+                for (int j = 0; j < ColSize; ++j)
+                {
+                    AddMatrix[i, j] = Matrix1[i, j] + Matrix2[i, j];
+                    rowTxt += AddMatrix[i, j] + "";
+                }
+                listBox3.Items.Add(rowTxt);
+            }
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int RowSize = int.Parse(textBox1.Text);
+            int ColSize = int.Parse (textBox2.Text);
+
+            AddMatrix = new int[RowSize, ColSize];
+            for (int i = 0;i < RowSize; ++i)
+            {
+                string rowTxt = "";
+                for (int j = 0;j < ColSize; ++j)
+                {
+                    AddMatrix[i,j] = Matrix1[i, j] - Matrix2[i, j];
+                    rowTxt += AddMatrix[i, j] + "";
+                }
+                listBox4.Items.Add(rowTxt);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            listBox3.Items.Clear();
+            listBox4.Items.Clear();
+        }
+    }
+}
