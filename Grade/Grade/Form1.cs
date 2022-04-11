aae9c82d8536a790e5f9d0414d0b91e72c35d630
PANOM-SOMAON <103121410+PANOM-SOMAON@users.noreply.github.com>
2022-04-11 20:12:06 +0700

    Add files via upload

---
+++ Grade/Grade/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Grade
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float T1 = float.Parse(textBox1.Text);
+            float T2 = float.Parse(textBox2.Text);
+            float T =T1 + T2;
+            if (T <= 100 && T >= 80) { MessageBox.Show("เกรดของคุณคือ A"); }
+            else if (T <= 79 && T >= 70) { MessageBox.Show("เกรดของคุณคือ B"); }
+            else if (T <= 69 && T >= 60) { MessageBox.Show("เกรดของคุณคือ C"); }
+            else if (T <= 59 && T >= 50) { MessageBox.Show("เกรดของคุณคือ D"); }
+            else if (T <= 49 && T > 0) { MessageBox.Show("เกรดของคุณคือ F"); }
+            else { MessageBox.Show("เกรด N"); }
+
+        }
+    }
+}
