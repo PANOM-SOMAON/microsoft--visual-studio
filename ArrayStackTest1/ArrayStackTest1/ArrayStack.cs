3e0e17cb31aa99143e82278b17a2f9399f208610
PANOM-SOMAON <103121410+PANOM-SOMAON@users.noreply.github.com>
2022-04-11 19:48:45 +0700

    Add files via upload

---
+++ ArrayStackTest1/ArrayStackTest1/ArrayStack.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrayStackTest
+{
+   public class ArrayStack
+    {
+        protected object[] data;
+        protected int pointer;
+
+        public ArrayStack(int capacity)
+        {
+            data = new object[capacity];
+            pointer = 0;
+        }
+        public bool isEmpty()
+        {
+            return data.Length == 0;
+        }
+        public void push (object ob)
+        {
+            if (pointer < data.Length)
+                data[pointer++] = ob;
+        }
+        public object pop()
+        {
+            if (isEmpty())
+                return null;
+            object oo = data[--pointer];
+            data[pointer] = null;
+            return oo;
+        }
+    }
+}
