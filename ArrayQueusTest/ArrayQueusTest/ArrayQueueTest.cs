05d66b9663fb31b7788139f3b2b1597d8d9af7fe
PANOM-SOMAON <103121410+PANOM-SOMAON@users.noreply.github.com>
2022-04-11 19:47:12 +0700

    Add files via upload

---
+++ ArrayQueusTest/ArrayQueusTest/ArrayQueueTest.cs
@@ -0,0 +1,31 @@
+﻿using ArrayQueusTest;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrayQueuTest
+{
+    public class ArrayQueueTest
+    {
+        public static void Main (string[] args)
+        {
+            ArrayQueue queue = new ArrayQueue(10);
+
+            for (int i = 0; i < 10; i++)
+            {
+                string objToQueue = "obj" + i;
+
+                Console.WriteLine("enqueueing: {0}", objToQueue);
+
+                queue.enqueue(objToQueue);
+            }
+
+            while (!queue.isEmpty())
+            {
+                string objFromQueue = (string)queue.dequeue();
+
+                Console.WriteLine("dequeued: {0}", objFromQueue);
+            }
+        }
+    }
+}
