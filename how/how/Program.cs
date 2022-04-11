f5a57148fd356ba988542468ab1e815eaec9138f
PANOM-SOMAON <103121410+PANOM-SOMAON@users.noreply.github.com>
2022-04-11 20:20:31 +0700

    Add files via upload

---
+++ how/how/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter type : ");
+            string type = Console.ReadLine();
+            
+            Console.WriteLine("Enter skin : ");
+           string skin = Console.ReadLine();
+           Console.WriteLine("Enter age : ");
+            string C = Console.ReadLine();
+            Console.WriteLine("type = {0}:", type);
+            Console.WriteLine("skin = {0}:", skin);
+            Console.WriteLine("age = {0}",C);
+            
+
+
+        }
+    }
+}
