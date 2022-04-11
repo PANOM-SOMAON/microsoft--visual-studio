e565038e8717786f561cb286240aa9f073a5ae37
PANOM-SOMAON <103121410+PANOM-SOMAON@users.noreply.github.com>
2022-04-11 20:08:14 +0700

    Add files via upload

---
+++ e/e/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace e_data
+{
+    class Program
+    {
+        static void Main(string[] args)
+          
+        {
+            string[] e_data05 = new string[] { "abc", "def", "ghi" };
+            for (int a = 0; a < 3; ++a)
+            {
+                Console.WriteLine("\t {0}", e_data05[a]);
+            }
+            
+        }
+    }
+}
