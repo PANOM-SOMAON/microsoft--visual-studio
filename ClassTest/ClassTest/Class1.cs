7d4b26d334ea2d23a79691e0a49080d0c81f550a
PANOM-SOMAON <103121410+PANOM-SOMAON@users.noreply.github.com>
2022-04-11 20:00:45 +0700

    Add files via upload

---
+++ ClassTest/ClassTest/Class1.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassTest
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            MyDatabase mdb1 = new MyDatabase();
+            mdb1.ShowCustomer();
+
+            MyDatabase mdb2 = new MyDatabase(101, "Miss Thailand", "5 Thai Rd");
+            mdb2.ShowCustomer();
+        }
+    }
+}
