8ef70e2237b6b5ff9c3d37581556436288652367
PANOM-SOMAON <103121410+PANOM-SOMAON@users.noreply.github.com>
2022-04-11 20:19:19 +0700

    Add files via upload

---
+++ Homework_Lab2.4/Homework_Lab2.4/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace TwoDiArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] intArr = new int[,] { { 1, 0, 0, 2 }, { 0, 1, 0, 0 }, { 0, 0, 1, 1 }, { 0, 1, 2, 1 } };
+            for (int i = 0; i < 5; ++i)
+            {
+                for (int j = 0; j < 5; ++j)
+                {
+                    Console.Write("\t {0}", intArr[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
